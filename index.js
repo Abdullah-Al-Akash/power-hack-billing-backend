dc661a884ee40d9fc9f2575b45594d12b4cc23a6
Abdullah Al Akash <aabdullahalakash@gmail.com>
2022-06-30 14:18:50 +0600

    Delete Information Successfully

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express()
 const port = process.env.PORT || 5000;
@@ -32,6 +32,14 @@ async function run() {
                         const newBilling = req.body;
                         const insertNewBilling = await informationsCollection.insertOne(newBilling);
                         res.send(insertNewBilling);
+                })
+
+                // Delete Bill Information:
+                app.delete('/api/delete-billing/:id', async (req, res) => {
+                        const id = req.params.id;
+                        const query = { _id: ObjectId(id) };
+                        const deletedBill = await informationsCollection.deleteOne(query);
+                        res.send(deletedBill);
                 })
         }
         finally {
